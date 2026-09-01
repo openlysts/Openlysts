86e0baeae4fa951d747aabb3c9bac4d820544342
Adilrafiq001 <adilrafiqd@gmail.com>
2026-09-01 14:34:03 +0530

    chore: ignore scratch, reports, and archive folders

--- scratch/check_dupes.js
+++
@@ -1,1 +0,0 @@
-import 'dotenv/config'; import { db } from '../server/db/index.js'; async function main() { const res = await db.query('SELECT COUNT(*), email_normalized FROM "User" GROUP BY email_normalized HAVING COUNT(*) > 1'); console.log(res.rows); process.exit(0); } main();
--- scratch/fix_user.cjs
+++
@@ -1,4 +0,0 @@
-const db = require('better-sqlite3')('data/openlyst.db');
-db.exec('ALTER TABLE "User" ADD COLUMN name TEXT');
-db.exec('ALTER TABLE "User" ADD COLUMN email TEXT');
-console.log('Done!');
